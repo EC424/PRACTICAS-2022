bff079f295030303ef0b466b0491f95aa8d03a78
EC424 <eduardo.casiano.348@gmail.com>
2022-06-10 12:50:30 -0500

    comentrio

--- bloque.js
+++ bloque.js
@@ -2,4 +2,4 @@ var t=new Date(2022,6,10);
 var futuro= new Date(2022,6,20);
 var diferencia=futuro-t;
 var difdedias=diferencia/(24*1000*60*60)
-alert(diferencia/(24*1000*60*60));
+alert(diferencia/(24*1000*60*60)); //editor de texto
