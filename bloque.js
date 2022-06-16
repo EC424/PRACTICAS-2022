6ac328d4c5da26a4bb42d349fdcb00bffceb8fd6
EC424 <eduardo.casiano.348@gmail.com>
2022-06-16 16:04:44 -0500

    1

---
+++ bloque.js
@@ -0,0 +1,13 @@
+var hoy=new Date();
+var año=hoy.getFullYear();
+var mes=hoy.getMonth()+1;
+var diaDelMes=hoy.getDate();
+var diaDeLaSemana=hoy.getDay()+1;
+var hora=hoy.getHours();
+var minutos=hoy.getMinutes()+1;
+var segundos=hoy.getSeconds()+1;
+var miliSegundos=hoy.getMilliseconds();
+var mili1970=hoy.getTime();
+function realizar(){
+   alert();
+ }
